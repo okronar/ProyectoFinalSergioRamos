d088349e044637ce103c85c07fd160023d93f34d
okronar <okronar@gmail.com>
2024-05-27 16:40:40 +0200

    cambio2 simulacro

--- ProyectosVisualStudio/ProyectoFinalAdministrador/ProyectoFinalBuscador/obj/Debug/net8.0-windows/ProyectoFinalAdministrador.AssemblyInfo.cs
+++ ProyectosVisualStudio/ProyectoFinalAdministrador/ProyectoFinalBuscador/obj/Debug/net8.0-windows/ProyectoFinalAdministrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalAdministrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f3507dd2c76b6ced3d8f17911ae7375433ffac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4400b77ac52db76381b9c8c12c05c5d2f3768b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalAdministrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalAdministrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
